00d57f86c59d7fc90d5cba77cedf41d4ec37e218
Your Name <gregyang@outlook.com>
2021-02-15 13:27:28 -0800

    basic arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "porgy",
+    about = "A CLI to genrate Prost rust code from protobuf."
+)]
+struct Opt {
+    #[structopt(long = "include", short = "I", parse(from_os_str))]
+    includes: Option<Vec<PathBuf>>,
+
+    #[structopt(required(true), parse(from_os_str))]
+    protos: Vec<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
 }
