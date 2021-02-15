73dc5f67b4465df93eb50ee094c24114b286aa01
Your Name <gregyang@outlook.com>
2021-02-15 15:03:10 -0800

    set OUT_DIR automatically

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::error::Error;
 use std::path::PathBuf;
 use structopt::StructOpt;
@@ -8,6 +9,15 @@ use structopt::StructOpt;
     about = "A CLI to genrate Prost rust code from protobuf."
 )]
 struct Opt {
+    #[structopt(
+        name = "OUTPUT_DIR",
+        env = "OUT_DIR",
+        long = "output",
+        short = "o",
+        default_value = "."
+    )]
+    output: String,
+
     #[structopt(
         name = "INCLUDE_PATH",
         long = "include",
@@ -23,7 +33,18 @@ struct Opt {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let opt = Opt::from_args();
+
+    let out_dir = "OUT_DIR";
+
+    let old_envvar = env::var(out_dir);
+    env::set_var(out_dir, opt.output);
+
     prost_build::compile_protos(&opt.protos, &opt.includes)?;
 
+    if let Ok(val) = old_envvar {
+        env::set_var(out_dir, val);
+    } else {
+        env::remove_var(out_dir);
+    }
     Ok(())
 }
